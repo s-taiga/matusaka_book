71d9c20bf813884fc1729f8e8350eb2919517e17
s-taiga <neverbefore351@gmail.com>
2024-01-01 16:44:18 +0900

    定理名修正

--- Main/book_1/chapter_1/section_2.lean
+++ Main/book_1/chapter_1/section_2.lean
@@ -10,108 +10,108 @@ section part_A
 本では普通の部分集合を⊆ではなく⊂で記載している
 -/
 
-theorem th_2_1 : A ∪ B = {x | x ∈ A ∨ x ∈ B} := by rfl
+theorem exp_2_1 : A ∪ B = {x | x ∈ A ∨ x ∈ B} := by rfl
 
-theorem th_2_2_1 : A ⊆ A ∪ B := by
+theorem exp_2_2_1 : A ⊆ A ∪ B := by
   intro x
   exact mem_union_left B
 
-theorem th_2_2_2 : B ⊆ A ∪ B := by
+theorem exp_2_2_2 : B ⊆ A ∪ B := by
   intro x
   exact mem_union_right A
 
-theorem th_2_3 (h₁ : A ⊆ C) (h₂ : B ⊆ C) : A ∪ B ⊆ C := by
+theorem exp_2_3 (h₁ : A ⊆ C) (h₂ : B ⊆ C) : A ∪ B ⊆ C := by
   rintro x (ha | hb)
   . exact h₁ ha
   . exact h₂ hb
 
-theorem th_2_4 : A ∪ A = A := union_self A
-theorem th_2_5 : A ∪ B = B ∪ A := union_comm A B
-theorem th_2_6 : (A ∪ B) ∪ C = A ∪ (B ∪ C) := union_assoc A B C
+theorem exp_2_4 : A ∪ A = A := union_self A
+theorem exp_2_5 : A ∪ B = B ∪ A := union_comm A B
+theorem exp_2_6 : (A ∪ B) ∪ C = A ∪ (B ∪ C) := union_assoc A B C
 
 example : ((A ∪ B) ∪ C) ∪ D = (A ∪ (B ∪ C)) ∪ D :=
   calc
     ((A ∪ B) ∪ C) ∪ D
-    _ = (A ∪ B) ∪ (C ∪ D) := th_2_6 (A ∪ B) C D
-    _ = A ∪ (B ∪ (C ∪ D)) := th_2_6 A B (C ∪ D)
-    _ = A ∪ ((B ∪ C) ∪ D) := by rw [← th_2_6 B C D]
-    _ = (A ∪ (B ∪ C)) ∪ D := by rw [← th_2_6 A (B ∪ C) D]
+    _ = (A ∪ B) ∪ (C ∪ D) := exp_2_6 (A ∪ B) C D
+    _ = A ∪ (B ∪ (C ∪ D)) := exp_2_6 A B (C ∪ D)
+    _ = A ∪ ((B ∪ C) ∪ D) := by rw [← exp_2_6 B C D]
+    _ = (A ∪ (B ∪ C)) ∪ D := by rw [← exp_2_6 A (B ∪ C) D]
 
-theorem th_2_7 : A ⊆ B ↔ A ∪ B = B := by
+theorem exp_2_7 : A ⊆ B ↔ A ∪ B = B := by
   constructor
   . intro h
-    have h₁ : A ∪ B ⊆ B := th_2_3 A B B h Subset.rfl
-    have h₁' : B ⊆ A ∪ B := th_2_2_2 A B
+    have h₁ : A ∪ B ⊆ B := exp_2_3 A B B h Subset.rfl
+    have h₁' : B ⊆ A ∪ B := exp_2_2_2 A B
     exact Subset.antisymm h₁ h₁'
   . intro h
     rw [← h]
-    exact th_2_2_1 A B
+    exact exp_2_2_1 A B
 
-theorem th_2_8 (h : A ⊆ B) : A ∪ C ⊆ B ∪ C := by
+theorem exp_2_8 (h : A ⊆ B) : A ∪ C ⊆ B ∪ C := by
   calc
     A ∪ C
-    _ ⊆ B ∪ (A ∪ C) := th_2_2_2 B (A ∪ C)
-    _ = (B ∪ A) ∪ C := by rw [← th_2_6]
-    _ = (A ∪ B) ∪ C := by rw [th_2_5 B A]
-    _ = B ∪ C := by rw [(th_2_7 A B).mp h]
+    _ ⊆ B ∪ (A ∪ C) := exp_2_2_2 B (A ∪ C)
+    _ = (B ∪ A) ∪ C := by rw [← exp_2_6]
+    _ = (A ∪ B) ∪ C := by rw [exp_2_5 B A]
+    _ = B ∪ C := by rw [(exp_2_7 A B).mp h]
 
-theorem th_2_9 : ∅ ∪ A = A := (th_2_7 ∅ A).mp (empty_subset A)
+theorem exp_2_9 : ∅ ∪ A = A := (exp_2_7 ∅ A).mp (empty_subset A)
 
 end part_A
 
 section part_B
 
-theorem th_2_2_1' : A ∩ B ⊆ A := by
+theorem exp_2_2_1' : A ∩ B ⊆ A := by
   intro x h
   exact mem_of_mem_inter_left h
 
-theorem th_2_2_2' : A ∩ B ⊆ B := by
+theorem exp_2_2_2' : A ∩ B ⊆ B := by
   intro x h
   exact mem_of_mem_inter_right h
 
-theorem th_2_3' (h₁ : C ⊆ A) (h₂ : C ⊆ B) : C ⊆ A ∩ B := by
+theorem exp_2_3' (h₁ : C ⊆ A) (h₂ : C ⊆ B) : C ⊆ A ∩ B := by
   intro x hc
   exact ⟨h₁ hc, h₂ hc⟩
 
-theorem th_2_4' : A ∩ A = A := inter_self A
-theorem th_2_5' : A ∩ B = B ∩ A := inter_comm A B
-theorem th_2_6' : (A ∩ B) ∩ C = A ∩ (B ∩ C) := inter_assoc A B C
+theorem exp_2_4' : A ∩ A = A := inter_self A
+theorem exp_2_5' : A ∩ B = B ∩ A := inter_comm A B
+theorem exp_2_6' : (A ∩ B) ∩ C = A ∩ (B ∩ C) := inter_assoc A B C
 
-theorem th_2_7' : A ⊆ B ↔ A ∩ B = A := by
+theorem exp_2_7' : A ⊆ B ↔ A ∩ B = A := by
   constructor
   . intro h
-    have h₁ : A ∩ B ⊆ A := th_2_2_1' A B
-    have h₂ : A ⊆ A ∩ B := th_2_3' A B A Subset.rfl h
+    have h₁ : A ∩ B ⊆ A := exp_2_2_1' A B
+    have h₂ : A ⊆ A ∩ B := exp_2_3' A B A Subset.rfl h
     exact Subset.antisymm h₁ h₂
   . intro h
     rw [← h]
-    exact th_2_2_2' A B
+    exact exp_2_2_2' A B
 
-theorem th_2_8' (h : A ⊆ B) : A ∩ C ⊆ B ∩ C := by
+theorem exp_2_8' (h : A ⊆ B) : A ∩ C ⊆ B ∩ C := by
   calc
     A ∩ C
-    _ = (A ∩ B) ∩ C := by rw [(th_2_7' A B).mp h]
-    _ = A ∩ (B ∩ C) := th_2_6' A B C
-    _ ⊆ B ∩ C := th_2_2_2' A (B ∩ C)
+    _ = (A ∩ B) ∩ C := by rw [(exp_2_7' A B).mp h]
+    _ = A ∩ (B ∩ C) := exp_2_6' A B C
+    _ ⊆ B ∩ C := exp_2_2_2' A (B ∩ C)
 
-theorem th_2_9' : ∅ ∩ A = ∅ := (th_2_7' ∅ A).mp (empty_subset A)
+theorem exp_2_9' : ∅ ∩ A = ∅ := (exp_2_7' ∅ A).mp (empty_subset A)
 
-theorem th_2_10 : (A ∪ B) ∩ C = (A ∩ C) ∪ (B ∩ C) := by
+theorem exp_2_10 : (A ∪ B) ∩ C = (A ∩ C) ∪ (B ∩ C) := by
   ext x
   simp
   tauto
 
-theorem th_2_10' : (A ∩ B) ∪ C = (A ∪ C) ∩ (B ∪ C) := by
+theorem exp_2_10' : (A ∩ B) ∪ C = (A ∪ C) ∩ (B ∪ C) := by
   ext x
   simp
   tauto
 
-theorem th_2_11 : (A ∪ B) ∩ A = A := by
+theorem exp_2_11 : (A ∪ B) ∩ A = A := by
   ext x
   simp
   tauto
 
-theorem th_2_11' : (A ∩ B) ∪ A = A := by
+theorem exp_2_11' : (A ∩ B) ∪ A = A := by
   ext x
   simp
   tauto
@@ -124,9 +124,9 @@ end part_C
 
 section part_D
 
-theorem th_2_12_1 : A ∪ Aᶜ = univ := union_compl_self A
-theorem th_2_12_2 : A ∩ Aᶜ = ∅ := inter_compl_self A
-theorem th_2_13 : Aᶜᶜ = A := by
+theorem exp_2_12_1 : A ∪ Aᶜ = univ := union_compl_self A
+theorem exp_2_12_2 : A ∩ Aᶜ = ∅ := inter_compl_self A
+theorem exp_2_13 : Aᶜᶜ = A := by
   ext x
   constructor
   . intro hcc
@@ -136,12 +136,12 @@ theorem th_2_13 : Aᶜᶜ = A := by
     apply not_mem_compl_iff.mpr
     exact h
 
-theorem th_2_14_1 : (∅ : Set S)ᶜ = univ := compl_empty
-theorem th_2_14_2 : (univ : Set S)ᶜ = ∅ := compl_univ
-theorem th_2_15 : A ⊆ B ↔ Bᶜ ⊆ Aᶜ := by
+theorem exp_2_14_1 : (∅ : Set S)ᶜ = univ := compl_empty
+theorem exp_2_14_2 : (univ : Set S)ᶜ = ∅ := compl_univ
+theorem exp_2_15 : A ⊆ B ↔ Bᶜ ⊆ Aᶜ := by
   simp [compl_subset_compl]
 
-theorem th_2_16 : (A ∪ B)ᶜ = Aᶜ ∩ Bᶜ := by
+theorem exp_2_16 : (A ∪ B)ᶜ = Aᶜ ∩ Bᶜ := by
   ext x
   calc
     x ∈ (A ∪ B)ᶜ
@@ -150,7 +150,7 @@ theorem th_2_16 : (A ∪ B)ᶜ = Aᶜ ∩ Bᶜ := by
     _ ↔ x ∈ Aᶜ ∧ x ∈ Bᶜ := by rw [mem_compl_iff, mem_compl_iff]
     _ ↔ x ∈ Aᶜ ∩ Bᶜ := by simp
 
-theorem th_2_16' : (A ∩ B)ᶜ = Aᶜ ∪ Bᶜ := by
+theorem exp_2_16' : (A ∩ B)ᶜ = Aᶜ ∪ Bᶜ := by
   ext x
   calc
     x ∈ (A ∩ B)ᶜ
@@ -169,10 +169,10 @@ section part_F
 
 variable {I} (A : I → Set S)
 
-theorem th_2_17 : ∀ i : I, A i ⊆ ⋃ j, A j := by sorry
-theorem th_2_18 : ∀ i : I, A i ⊆ C → ⋃ j, A j ⊆ C := by sorry
-theorem th_2_17' : ∀ i : I, ⋂ j, A j ⊆ A i := by sorry
-theorem th_2_18' : ∀ i : I, C ⊆ A i → C ⊆ ⋂ j, A j := by sorry
+theorem exp_2_17 : ∀ i : I, A i ⊆ ⋃ j, A j := by sorry
+theorem exp_2_18 : ∀ i : I, A i ⊆ C → ⋃ j, A j ⊆ C := by sorry
+theorem exp_2_17' : ∀ i : I, ⋂ j, A j ⊆ A i := by sorry
+theorem exp_2_18' : ∀ i : I, C ⊆ A i → C ⊆ ⋂ j, A j := by sorry
 
 end part_F
 
@@ -184,19 +184,19 @@ theorem ex_lemma_2 : A ∪ (B ∩ C) = (A ∪ B) ∩ (A ∪ C) := union_distrib_
 theorem ex_1_a : (A ∪ B) ∩ (A ∪ Bᶜ) = A :=
   calc
     (A ∪ B) ∩ (A ∪ Bᶜ)
-    _ = (B ∪ A) ∩ (Bᶜ ∪ A) := by rw [th_2_5 A B, th_2_5 A Bᶜ]
-    _ = (B ∩ Bᶜ) ∪ A := by rw [← th_2_10']
-    _ = ∅ ∪ A := by rw [th_2_12_2]
-    _ = A := th_2_9 A
+    _ = (B ∪ A) ∩ (Bᶜ ∪ A) := by rw [exp_2_5 A B, exp_2_5 A Bᶜ]
+    _ = (B ∩ Bᶜ) ∪ A := by rw [← exp_2_10']
+    _ = ∅ ∪ A := by rw [exp_2_12_2]
+    _ = A := exp_2_9 A
 
 theorem ex_1_b : (A ∪ B) ∩ (Aᶜ ∪ B) ∩ (A ∪ Bᶜ) = A ∩ B :=
   calc
     (A ∪ B) ∩ (Aᶜ ∪ B) ∩ (A ∪ Bᶜ)
-    _ = (A ∪ B) ∩ (A ∪ Bᶜ) ∩ (Aᶜ ∪ B) := by rw [th_2_6', th_2_5' (Aᶜ ∪ B), ← th_2_6']
-    _ = (Aᶜ ∪ B) ∩ A := by rw [ex_1_a, th_2_5']
-    _ = (Aᶜ ∩ A) ∪ (B ∩ A) := by rw [th_2_10]
-    _ = ∅ ∪ (A ∩ B) := by rw [th_2_5', th_2_12_2, th_2_5']
-    _ = A ∩ B := th_2_9 (A ∩ B)
+    _ = (A ∪ B) ∩ (A ∪ Bᶜ) ∩ (Aᶜ ∪ B) := by rw [exp_2_6', exp_2_5' (Aᶜ ∪ B), ← exp_2_6']
+    _ = (Aᶜ ∪ B) ∩ A := by rw [ex_1_a, exp_2_5']
+    _ = (Aᶜ ∩ A) ∪ (B ∩ A) := by rw [exp_2_10]
+    _ = ∅ ∪ (A ∩ B) := by rw [exp_2_5', exp_2_12_2, exp_2_5']
+    _ = A ∩ B := exp_2_9 (A ∩ B)
 
 theorem ex_2_1 : A ∩ B = ∅ ↔ B ⊆ Aᶜ := by
   constructor
@@ -204,45 +204,45 @@ theorem ex_2_1 : A ∩ B = ∅ ↔ B ⊆ Aᶜ := by
     have : B ∩ Aᶜ = B :=
       calc
         B ∩ Aᶜ
-        _ = (B ∩ Aᶜ) ∪ ∅ := by rw [th_2_5, th_2_9]
-        _ = (Aᶜ ∩ B) ∪ (A ∩ B) := by rw [← h, th_2_5']
-        _ = (Aᶜ ∪ A) ∩ B := by rw [← th_2_10]
-        _ = univ ∩ B := by rw [th_2_5, th_2_12_1]
+        _ = (B ∩ Aᶜ) ∪ ∅ := by rw [exp_2_5, exp_2_9]
+        _ = (Aᶜ ∩ B) ∪ (A ∩ B) := by rw [← h, exp_2_5']
+        _ = (Aᶜ ∪ A) ∩ B := by rw [← exp_2_10]
+        _ = univ ∩ B := by rw [exp_2_5, exp_2_12_1]
         _ = B := univ_inter B
     rw [← this]
-    apply th_2_2_2' B Aᶜ
+    apply exp_2_2_2' B Aᶜ
   . intro h
     have h₁ : ∅ ⊆ A ∩ B := empty_subset (A ∩ B)
     have h₂ : A ∩ B ⊆ ∅ :=
       calc
         A ∩ B
-        _ ⊆ A ∩ Aᶜ := by rw [th_2_5', th_2_5' A]; exact th_2_8' B Aᶜ A h
-        _ = ∅ := th_2_12_2 A
+        _ ⊆ A ∩ Aᶜ := by rw [exp_2_5', exp_2_5' A]; exact exp_2_8' B Aᶜ A h
+        _ = ∅ := exp_2_12_2 A
     exact Subset.antisymm h₂ h₁
 
 theorem ex_2_2 : B ⊆ Aᶜ ↔ A ⊆ Bᶜ :=
   calc
     B ⊆ Aᶜ
-    _ ↔ Aᶜᶜ ⊆ Bᶜ := by rw [th_2_15]
-    _ ↔ A ⊆ Bᶜ := by rw [th_2_13]
+    _ ↔ Aᶜᶜ ⊆ Bᶜ := by rw [exp_2_15]
+    _ ↔ A ⊆ Bᶜ := by rw [exp_2_13]
 
 theorem ex_3_a_1 : A \ B = (A ∪ B) \ B := by rw [← union_diff_right]
-theorem ex_3_a_2 : A \ B = A \ (A ∩ B) := by rw [th_2_5', diff_inter_self_eq_diff]
+theorem ex_3_a_2 : A \ B = A \ (A ∩ B) := by rw [exp_2_5', diff_inter_self_eq_diff]
 theorem ex_3_a_3 : A \ B = A ∩ Bᶜ := diff_eq A B
 
 theorem ex_lemma_3 : A ∩ B = A ↔ A ⊆ B := by
   constructor
   . intro h
     rw [← h]
-    exact th_2_2_2' A B
+    exact exp_2_2_2' A B
   . intro h
-    have h₁ : A ∩ B ⊆ A := th_2_2_1' A B
+    have h₁ : A ∩ B ⊆ A := exp_2_2_1' A B
     have h₂ : A ⊆ A ∩ B :=
       calc
         A
-        _ = A ∩ A := (th_2_4' A).symm
-        _ ⊆ B ∩ A := th_2_8' A B A h
-        _ = A ∩ B := th_2_5' B A
+        _ = A ∩ A := (exp_2_4' A).symm
+        _ ⊆ B ∩ A := exp_2_8' A B A h
+        _ = A ∩ B := exp_2_5' B A
     exact Subset.antisymm h₁ h₂
 
 theorem ex_3_b : A \ B = A ↔ A ∩ B = ∅ :=
@@ -258,21 +258,21 @@ theorem ex_3_c : A \ B = ∅ ↔ A ⊆ B :=
     A \ B = ∅
     _ ↔ A ∩ Bᶜ = ∅ := by rw [ex_3_a_3]
     _ ↔ Bᶜ ⊆ Aᶜ := ex_2_1 A Bᶜ
-    _ ↔ A ⊆ B := (th_2_15 A B).symm
+    _ ↔ A ⊆ B := (exp_2_15 A B).symm
 
 theorem ex_4_a : A \ (B ∪ C) = (A \ B) ∩ (A \ C) :=
   calc
     A \ (B ∪ C)
     _ = A ∩ (B ∪ C)ᶜ := ex_3_a_3 A (B ∪ C)
-    _ = (A ∩ A) ∩ (Bᶜ ∩ Cᶜ) := by nth_rewrite 1 [← th_2_4' A]; rw [th_2_16]
-    _ = (A ∩ Bᶜ) ∩ (A ∩ Cᶜ) := by rw [th_2_6', ← th_2_6' A Bᶜ]; nth_rewrite 1 [th_2_5' A Bᶜ]; rw [th_2_6', ← th_2_6' A]
+    _ = (A ∩ A) ∩ (Bᶜ ∩ Cᶜ) := by nth_rewrite 1 [← exp_2_4' A]; rw [exp_2_16]
+    _ = (A ∩ Bᶜ) ∩ (A ∩ Cᶜ) := by rw [exp_2_6', ← exp_2_6' A Bᶜ]; nth_rewrite 1 [exp_2_5' A Bᶜ]; rw [exp_2_6', ← exp_2_6' A]
     _ = (A \ B) ∩ (A \ C) := by rw [ex_3_a_3 A B, ex_3_a_3]
 
 theorem ex_4_b : A \ (B ∩ C) = (A \ B) ∪ (A \ C) :=
   calc
     A \ (B ∩ C)
     _ = A ∩ (B ∩ C)ᶜ := ex_3_a_3 A (B ∩ C)
-    _ = A ∩ (Bᶜ ∪ Cᶜ) := by rw [th_2_16']
+    _ = A ∩ (Bᶜ ∪ Cᶜ) := by rw [exp_2_16']
     _ = (A ∩ Bᶜ) ∪ (A ∩ Cᶜ) := by rw [ex_lemma_1]
     _ = (A \ B) ∪ (A \ C) := by rw [ex_3_a_3 A B, ex_3_a_3]
 
@@ -280,54 +280,54 @@ theorem ex_4_c : (A ∪ B) \ C = (A \ C) ∪ (B \ C) :=
   calc
     (A ∪ B) \ C
     _ = (A ∪ B) ∩ Cᶜ := ex_3_a_3 (A ∪ B) C
-    _ = (A ∩ Cᶜ) ∪ (B ∩ Cᶜ) := th_2_10 A B Cᶜ
+    _ = (A ∩ Cᶜ) ∪ (B ∩ Cᶜ) := exp_2_10 A B Cᶜ
     _ = (A \ C) ∪ (B \ C) := by rw [ex_3_a_3 A C, ex_3_a_3]
 
 theorem ex_4_d : (A ∩ B) \ C = (A \ C) ∩ (B \ C) :=
   calc
     (A ∩ B) \ C
     _ = (A ∩ B) ∩ Cᶜ := ex_3_a_3 (A ∩ B) C
-    _ = (A ∩ B) ∩ (Cᶜ ∩ Cᶜ) := by rw [th_2_4' Cᶜ]
-    _ = (A ∩ Cᶜ) ∩ (B ∩ Cᶜ) := by rw [th_2_6', ← th_2_6' B Cᶜ]; nth_rewrite 1 [th_2_5' B]; rw [th_2_6', ← th_2_6']
+    _ = (A ∩ B) ∩ (Cᶜ ∩ Cᶜ) := by rw [exp_2_4' Cᶜ]
+    _ = (A ∩ Cᶜ) ∩ (B ∩ Cᶜ) := by rw [exp_2_6', ← exp_2_6' B Cᶜ]; nth_rewrite 1 [exp_2_5' B]; rw [exp_2_6', ← exp_2_6']
     _ = (A \ C) ∩ (B \ C) := by rw [ex_3_a_3 A C, ex_3_a_3]
 
 theorem ex_4_e : A ∩ (B \ C) = (A ∩ B) \ (A ∩ C) :=
   calc
     A ∩ (B \ C)
     _ = A ∩ (B ∩ Cᶜ) := by rw [ex_3_a_3]
-    _ = ∅ ∪ (A ∩ (B ∩ Cᶜ)) := by rw [th_2_9]
-    _ = (∅ ∩ B) ∪ (A ∩ (B ∩ Cᶜ)) := by rw [th_2_9']
-    _ = (A ∩ Aᶜ ∩ B) ∪ (A ∩ (B ∩ Cᶜ)) := by rw [th_2_12_2]
-    _ = (Aᶜ ∩ (A ∩ B)) ∪ (Cᶜ ∩ (A ∩ B)) := by rw [th_2_5' A, th_2_6', th_2_5' A (B ∩ Cᶜ), th_2_5' B Cᶜ, th_2_6', th_2_5' B A]
-    _ = (Aᶜ ∪ Cᶜ) ∩ (A ∩ B) := by rw [th_2_10]
-    _ = (A ∩ B) ∩ (Aᶜ ∪ Cᶜ) := by rw [th_2_5']
-    _ = (A ∩ B) ∩ (A ∩ C)ᶜ := by rw [th_2_16']
+    _ = ∅ ∪ (A ∩ (B ∩ Cᶜ)) := by rw [exp_2_9]
+    _ = (∅ ∩ B) ∪ (A ∩ (B ∩ Cᶜ)) := by rw [exp_2_9']
+    _ = (A ∩ Aᶜ ∩ B) ∪ (A ∩ (B ∩ Cᶜ)) := by rw [exp_2_12_2]
+    _ = (Aᶜ ∩ (A ∩ B)) ∪ (Cᶜ ∩ (A ∩ B)) := by rw [exp_2_5' A, exp_2_6', exp_2_5' A (B ∩ Cᶜ), exp_2_5' B Cᶜ, exp_2_6', exp_2_5' B A]
+    _ = (Aᶜ ∪ Cᶜ) ∩ (A ∩ B) := by rw [exp_2_10]
+    _ = (A ∩ B) ∩ (Aᶜ ∪ Cᶜ) := by rw [exp_2_5']
+    _ = (A ∩ B) ∩ (A ∩ C)ᶜ := by rw [exp_2_16']
     _ = (A ∩ B) \ (A ∩ C) := by rw [ex_3_a_3]
 
 theorem ex_5_a : (A \ B) \ C = A \ (B ∪ C) :=
   calc
     (A \ B) \ C
     _ = (A ∩ Bᶜ) ∩ Cᶜ := by rw [ex_3_a_3, ex_3_a_3]
-    _ = A ∩ (Bᶜ ∩ Cᶜ) := by rw [th_2_6']
-    _ = A ∩ (B ∪ C)ᶜ := by rw [th_2_16]
+    _ = A ∩ (Bᶜ ∩ Cᶜ) := by rw [exp_2_6']
+    _ = A ∩ (B ∪ C)ᶜ := by rw [exp_2_16]
     _ = A \ (B ∪ C) := by rw [ex_3_a_3]
 
 theorem ex_5_b : A \ (B \ C) = (A \ B) ∪ (A ∩ C) :=
   calc
     A \ (B \ C)
     _ = A ∩ (B ∩ Cᶜ)ᶜ := by rw [ex_3_a_3, ex_3_a_3]
-    _ = A ∩ (Bᶜ ∪ Cᶜᶜ) := by rw [th_2_16']
-    _ = A ∩ (Bᶜ ∪ C) := by rw [th_2_13]
+    _ = A ∩ (Bᶜ ∪ Cᶜᶜ) := by rw [exp_2_16']
+    _ = A ∩ (Bᶜ ∪ C) := by rw [exp_2_13]
     _ = (A ∩ Bᶜ) ∪ (A ∩ C) := by rw [ex_lemma_1]
     _ = (A \ B) ∪ (A ∩ C) := by rw [ex_3_a_3]
 
 theorem ex_6 : A ⊆ C → ∀ B, A ∪ (B ∩ C) = (A ∪ B) ∩ C := by
   intro h B
   have : A ∪ C = C := by
-    have h₁ : C ⊆ A ∪ C := th_2_2_2 A C
+    have h₁ : C ⊆ A ∪ C := exp_2_2_2 A C
     have h₂ : A ∪ C ⊆ C := by
-      nth_rewrite 2 [← th_2_4 C]
-      exact th_2_8 A C C h
+      nth_rewrite 2 [← exp_2_4 C]
+      exact exp_2_8 A C C h
     exact Subset.antisymm h₂ h₁
   calc
     A ∪ (B ∩ C)
@@ -336,42 +336,42 @@ theorem ex_6 : A ⊆ C → ∀ B, A ∪ (B ∩ C) = (A ∪ B) ∩ C := by
 
 theorem ex_lemma_4 : A ∆ B = (A \ B) ∪ (B \ A) := symmDiff_def A B
 theorem ex_lemma_5 : A ∆ B = (A ∩ Bᶜ) ∪ (Aᶜ ∩ B) := by
-  rw [ex_lemma_4, ex_3_a_3, ex_3_a_3, th_2_5' B]
+  rw [ex_lemma_4, ex_3_a_3, ex_3_a_3, exp_2_5' B]
 
 theorem ex_7_a : A ∆ B = B ∆ A :=
   calc
     A ∆ B
     _ = (A \ B) ∪ (B \ A) := ex_lemma_4 A B
-    _ = (B \ A) ∪ (A \ B) := th_2_5 (A \ B) (B \ A)
+    _ = (B \ A) ∪ (A \ B) := exp_2_5 (A \ B) (B \ A)
     _ = B ∆ A := ex_lemma_4 B A
 
 theorem ex_7_b : A ∆ B = (A ∪ B) \ (A ∩ B) :=
   calc
     A ∆ B
     _ = (A ∩ Bᶜ) ∪ (Aᶜ ∩ B) := ex_lemma_5 A B
-    _ = (A ∪ (Aᶜ ∩ B)) ∩ (Bᶜ ∪ (Aᶜ ∩ B)) := by rw [th_2_10']
+    _ = (A ∪ (Aᶜ ∩ B)) ∩ (Bᶜ ∪ (Aᶜ ∩ B)) := by rw [exp_2_10']
     _ = ((A ∪ Aᶜ) ∩ (A ∪ B)) ∩ ((Bᶜ ∪ Aᶜ) ∩ (Bᶜ ∪ B)) := by rw [ex_lemma_2, ex_lemma_2]
-    _ = (univ ∩ (A ∪ B)) ∩ ((Bᶜ ∪ Aᶜ) ∩ univ) := by rw [th_2_12_1, th_2_5 Bᶜ B, th_2_12_1]
+    _ = (univ ∩ (A ∪ B)) ∩ ((Bᶜ ∪ Aᶜ) ∩ univ) := by rw [exp_2_12_1, exp_2_5 Bᶜ B, exp_2_12_1]
     _ = (A ∪ B) ∩ (Bᶜ ∪ Aᶜ) := by rw [univ_inter, inter_univ]
-    _ = (A ∪ B) ∩ (A ∩ B)ᶜ := by rw [th_2_16', th_2_5 Bᶜ]
+    _ = (A ∪ B) ∩ (A ∩ B)ᶜ := by rw [exp_2_16', exp_2_5 Bᶜ]
     _ = (A ∪ B) \ (A ∩ B) := by rw [ex_3_a_3]
 
 theorem ex_lemma_6 : (A ∆ B)ᶜ = (Aᶜ ∩ Bᶜ) ∪ (A ∩ B) :=
   calc
     (A ∆ B)ᶜ
     _ = ((A ∪ B) ∩ (A ∩ B)ᶜ)ᶜ := by rw [ex_7_b, ex_3_a_3]
-    _ = (A ∪ B)ᶜ ∪ (A ∩ B)ᶜᶜ := by rw [th_2_16']
-    _ = (A ∪ B)ᶜ ∪ (A ∩ B) := by rw [th_2_13]
-    _ = (Aᶜ ∩ Bᶜ) ∪ (A ∩ B) := by rw [th_2_16]
+    _ = (A ∪ B)ᶜ ∪ (A ∩ B)ᶜᶜ := by rw [exp_2_16']
+    _ = (A ∪ B)ᶜ ∪ (A ∩ B) := by rw [exp_2_13]
+    _ = (Aᶜ ∩ Bᶜ) ∪ (A ∩ B) := by rw [exp_2_16]
 
 theorem ex_7_c : (A ∆ B) ∆ C = A ∆ (B ∆ C) :=
   calc
     (A ∆ B) ∆ C
     _ = (A ∆ B) ∩ Cᶜ ∪ (A ∆ B)ᶜ ∩ C := by rw [ex_lemma_5]
     _ = (A ∩ Bᶜ ∪ Aᶜ ∩ B) ∩ Cᶜ ∪ ((Aᶜ ∩ Bᶜ) ∪ (A ∩ B)) ∩ C := by rw [ex_lemma_6, ex_lemma_5]
-    _ = (A ∩ Bᶜ ∩ Cᶜ) ∪ (Aᶜ ∩ B ∩ Cᶜ) ∪ ((Aᶜ ∩ Bᶜ ∩ C) ∪ (A ∩ B ∩ C)) := by rw [th_2_10, th_2_10]
-    _ = (A ∩ Bᶜ ∩ Cᶜ) ∪ (A ∩ B ∩ C) ∪ ((Aᶜ ∩ B ∩ Cᶜ) ∪ (Aᶜ ∩ Bᶜ ∩ C)) := by rw [th_2_6, ← th_2_6 (Aᶜ ∩ B ∩ Cᶜ), th_2_5 _ (A ∩ B ∩ C), ← th_2_6]
-    _ = A ∩ (Bᶜ ∩ Cᶜ ∪ B ∩ C) ∪ Aᶜ ∩ ((B ∩ Cᶜ) ∪ (Bᶜ ∩ C)) := by rw [th_2_6' A, th_2_6' A, th_2_6' Aᶜ, th_2_6' Aᶜ, ex_lemma_1 A, ex_lemma_1 Aᶜ]
+    _ = (A ∩ Bᶜ ∩ Cᶜ) ∪ (Aᶜ ∩ B ∩ Cᶜ) ∪ ((Aᶜ ∩ Bᶜ ∩ C) ∪ (A ∩ B ∩ C)) := by rw [exp_2_10, exp_2_10]
+    _ = (A ∩ Bᶜ ∩ Cᶜ) ∪ (A ∩ B ∩ C) ∪ ((Aᶜ ∩ B ∩ Cᶜ) ∪ (Aᶜ ∩ Bᶜ ∩ C)) := by rw [exp_2_6, ← exp_2_6 (Aᶜ ∩ B ∩ Cᶜ), exp_2_5 _ (A ∩ B ∩ C), ← exp_2_6]
+    _ = A ∩ (Bᶜ ∩ Cᶜ ∪ B ∩ C) ∪ Aᶜ ∩ ((B ∩ Cᶜ) ∪ (Bᶜ ∩ C)) := by rw [exp_2_6' A, exp_2_6' A, exp_2_6' Aᶜ, exp_2_6' Aᶜ, ex_lemma_1 A, ex_lemma_1 Aᶜ]
     _ = A ∩ (B ∆ C)ᶜ ∪ Aᶜ ∩ (B ∆ C) := by rw [ex_lemma_6, ex_lemma_5]
     _ = A ∆ (B ∆ C) := by rw [← ex_lemma_5]
 
@@ -380,36 +380,36 @@ theorem ex_7_d : A ∩ (B ∆ C) = (A ∩ B) ∆ (A ∩ C) :=
     A ∩ (B ∆ C)
     _ = A ∩ ((B ∪ C) \ (B ∩ C)) := by rw [ex_7_b]
     _ = (A ∩ (B ∪ C)) \ (A ∩ (B ∩ C)) := by rw [ex_4_e]
-    _ = (A ∩ B ∪ A ∩ C) \ ((A ∩ A) ∩ (B ∩ C)) := by rw [ex_lemma_1, th_2_4']
-    _ = (A ∩ B ∪ A ∩ C) \ ((A ∩ B) ∩ (A ∩ C)) := by rw [th_2_6', ← th_2_6' A B, th_2_5' (A ∩ B), ← th_2_6', th_2_5' (A ∩ C)]
+    _ = (A ∩ B ∪ A ∩ C) \ ((A ∩ A) ∩ (B ∩ C)) := by rw [ex_lemma_1, exp_2_4']
+    _ = (A ∩ B ∪ A ∩ C) \ ((A ∩ B) ∩ (A ∩ C)) := by rw [exp_2_6', ← exp_2_6' A B, exp_2_5' (A ∩ B), ← exp_2_6', exp_2_5' (A ∩ C)]
     _ = (A ∩ B) ∆ (A ∩ C) := by rw [ex_7_b]
 
 theorem ex_8_a : A ∆ ∅ = A :=
   calc
     A ∆ ∅
     _ = (A ∩ ∅ᶜ) ∪ (Aᶜ ∩ ∅) := by rw [ex_lemma_5]
-    _ = (A ∩ univ) ∪ ∅ := by rw [th_2_14_1, th_2_5' Aᶜ, th_2_9']
-    _ = A := by rw [inter_univ, th_2_5, th_2_9]
+    _ = (A ∩ univ) ∪ ∅ := by rw [exp_2_14_1, exp_2_5' Aᶜ, exp_2_9']
+    _ = A := by rw [inter_univ, exp_2_5, exp_2_9]
 
 theorem ex_8_b : A ∆ univ = Aᶜ :=
   calc
     A ∆ univ
     _ = (A ∩ univᶜ) ∪ (Aᶜ ∩ univ) := by rw [ex_lemma_5]
-    _ = (A ∩ ∅) ∪ Aᶜ := by rw [th_2_14_2, inter_univ]
-    _ = Aᶜ := by rw [th_2_5', th_2_9', th_2_9]
+    _ = (A ∩ ∅) ∪ Aᶜ := by rw [exp_2_14_2, inter_univ]
+    _ = Aᶜ := by rw [exp_2_5', exp_2_9', exp_2_9]
 
 theorem ex_8_c : A ∆ A = ∅ :=
   calc
     A ∆ A
     _ = (A ∪ A) \ (A ∩ A) := by rw [ex_7_b]
-    _ = A \ A := by rw [th_2_4, th_2_4']
+    _ = A \ A := by rw [exp_2_4, exp_2_4']
     _ = ∅ := by rw [diff_self]
 
 theorem ex_8_d : A ∆ Aᶜ = univ :=
   calc
     A ∆ Aᶜ
     _ = (A ∪ Aᶜ) \ (A ∩ Aᶜ) := by rw [ex_7_b]
-    _ = univ \ ∅ := by rw [th_2_12_1, th_2_12_2]
+    _ = univ \ ∅ := by rw [exp_2_12_1, exp_2_12_2]
     _ = univ := by rw [diff_empty]
 
 theorem ex_9 (A₁ A₂ B₁ B₂ : Set S) : A₁ ∆ A₂ = B₁ ∆ B₂ → A₁ ∆ B₁ = A₂ ∆ B₂ := by
